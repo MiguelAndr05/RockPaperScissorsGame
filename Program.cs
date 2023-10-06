c4246123551a0af14cf2e0b360cacbaf00abd6ba
NotStrixx <miguelfandrade2005@gmail.com>
2023-10-06 02:00:48 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class RockPaperScissors
+{
+    static void Main()
+    {
+        string userInput;
+
+
+        do
+        {
+            Console.WriteLine("What would you like to pick? (Rock, Paper, or Scissors)");
+            userInput = Console.ReadLine();
+
+            if (userInput.ToLower() == "rock" || userInput.ToLower() == "paper" || userInput.ToLower() == "scissors")
+            {
+                
+                Random random = new Random();
+                int computerChoice = random.Next(1, 4);
+                string aiPick = "";
+
+               
+                
+
+                Console.WriteLine("Computer Picks: " + aiPick);
+
+                
+                
+               
+
+                Console.WriteLine("If you would like to play again, please type 'Play'");
+                Console.WriteLine("If you would like to stop playing, type 'Exit'");
+                userInput = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Please pick Rock, Paper, or Scissors.");
+            }
+
+        } while (userInput.ToLower() == "play");
+
+        if (userInput.ToLower() == "exit")
+        {
+            Console.WriteLine("Thanks for playing!");
+        
+        }
+    }
+}   
+   
